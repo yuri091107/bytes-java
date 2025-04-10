83fe45e4995eeacf4cb9de50b8cf9897bd239d85
Zx <148573313+yuri091107@users.noreply.github.com>
2025-04-10 01:28:56 -0300

    primeiro commit

---
+++ armazenar valores com base no seus bytes/Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+    
+    
+    public static void main(String[] args) {
+    
+        //Alertando usuário para interação
+        System.out.println("Digite o numero");
+    
+        //Instanciando a classe
+        Variaveis variaveis = new Variaveis();
+        
+        //Chamando métodos
+        variaveis.Byte();
+        variaveis.Short();
+        variaveis.Inteiro();
+        variaveis.Long();
+    
+    }
+
+}
